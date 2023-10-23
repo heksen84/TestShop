cb17a677acc0f75bffaadb60144263ff6b3275a0
Ilya Bobkov <ilyabobkovprog@gmail.com>
2023-10-23 18:47:57 +0600

    upd

--- database/migrations/2023_10_23_083807_create_products_table.php
+++ database/migrations/2023_10_23_083807_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
+            $table->decimal('price');
+            $table->boolean('available')->default(false);
             $table->timestamps();
         });
     }
