3c9f991d25dfd4010f5e46a251b1b25d3beaedad
Ilya Bobkov <ilyabobkovprog@gmail.com>
2023-10-24 16:13:52 +0600

    upd

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\File;
-use App\Models\Product;
 use App\Http\Requests\ProductRequest;
+use App\Models\Product;
 use App\Models\Category;
 
 class ProductController extends Controller
