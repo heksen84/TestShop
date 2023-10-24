8eff333c2cbfde223b547b435216c4d026761a6f
Ilya Bobkov <ilyabobkovprog@gmail.com>
2023-10-24 12:19:36 +0600

    upd

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class ProductController extends Controller
 {
@@ -98,13 +99,56 @@ class ProductController extends Controller
      *         name="id",
      *         in="path",
      *         description="id продукта"     
-     *     )     
+     *     ),
+     * *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      description="id категории",
+     *                      property="category_id",
+     *                      type="number",
+     *			            default=1
+     *                 ),
+     *                 @OA\Property(
+     *                      description="название продукта",
+     *                      property="name",
+     *                      type="string",
+     *			            default="Аксессуары"
+     *                 ),
+     *                 @OA\Property(
+     *                      description="описание",
+     *                      property="description",
+     *                      type="string",
+     *			            default="Описание продукта"
+     *                 ),    
+     *                 @OA\Property(
+     *                      description="изображение",
+     *                      property="image",
+     *                      type="file",
+     *                 ),
+     *                 @OA\Property(
+     *                      description="цена",
+     *                      property="price",
+     *                      type="number",
+     *			            default=100000
+     *                 ),
+     *                 @OA\Property(
+     *                      description="в наличии",
+     *                      property="available",
+     *                      type="boolean",
+     *                 )
+     * )))     
      * )     
      * )     
      */
-    public function update($id)
+    public function update($id, Request $request)
     {
         $products = Product::findOrFail($id);
+        
+        if (File::delete(public_path() . '/images/' . basename($products->image)))
+            $products['image'] = url('') . '/' . $request->image->store('images', 'public');
+
         return $products;
     }
 
